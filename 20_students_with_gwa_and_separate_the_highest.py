c8e1b31e4821f5d46c824f15eac74a75ff42c773
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-27 03:53:25 +0800

    try to put exemption

--- 20_students_with_gwa_and_separate_the_highest.py
+++ 20_students_with_gwa_and_separate_the_highest.py
@@ -4,17 +4,18 @@ class Separate:
     def find_highest_gwa(self):
         highest_student = None
         highest_gwa = 1.0
-        with open(self.students_file,'r') as student_file:
-            for line in student_file:
-                line = line.strip()
-                name = line.split()[0]
-                gwa = float(line.split()[1])
-                if gwa > highest_gwa:
-                    highest_gwa = gwa
-                    highest_student = name
+        try:
+            with open(self.students_file,'r') as student_file:
+                for line in student_file:
+                    line = line.strip()
+                    name = line.split()[0]
+                    gwa = float(line.split()[1])
+                    if gwa > highest_gwa:
+                        highest_gwa = gwa
+                        highest_student = name
         if highest_student:
             print(f"{highest_student.capitalize()} is the highest, with the highest gwa: {highest_gwa}")
         else:
             print(f"there is no highest gwa")
-    
+
 
