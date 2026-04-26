43b21c21515b4327aa7710e11cb755e97f9cce88
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-27 01:45:55 +0800

    sort the file

--- seperate_even_and_odd_numbers_in_file_while_in_oop.py
+++ seperate_even_and_odd_numbers_in_file_while_in_oop.py
@@ -1,10 +1,13 @@
 class Sorter:
-    def __init__(self,numbers.txt,):
-    def main_process(my_file):
+    def __init__(self,numbers_file,even_output,odd_output):
+        self.numbers_file = numbers_file
+        self.even_output = even_output
+        self.odd_output = odd_output
+    def main_process(self):
         try:
-            with(open("numbers.txt","r")) as my_file:
+            with(open(self.numbers_file,"r")) as my_file:
                 numbers = [int(line.strip()) for line in my_file if line.strip()]
-            with(open("even_output.txt","w")) as even_file,open("odd_output.txt","w") as odd_file:
+            with(open(self.even_output,"w")) as even_file,open(self.odd_output,"w") as odd_file:
                 for num in numbers:
                     if num % 2 == 0:
                         even_file.write(f'{num}\n')
@@ -15,6 +18,10 @@ class Sorter:
             print(f"The file {my_file} was not found.")
         except ValueError:
             print(f"The file contains non-integer values.")
+if __name__ == "__main__":
+    sorter = Sorter("numbers.txt","even_output.txt","odd_output.txt")
+    sorter.main_process()
+    
 
 
 
