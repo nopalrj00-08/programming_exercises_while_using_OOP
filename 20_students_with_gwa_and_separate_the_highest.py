bc2aa7cc89cdc9d227f33bf278db2b0ac6bfff56
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-27 03:34:46 +0800

    find highest gwa intro

--- 20_students_with_gwa_and_separate_the_highest.py
+++ 20_students_with_gwa_and_separate_the_highest.py
@@ -1,4 +1,8 @@
 class Separate:
     def __init__(self,students_file):
         self.students_file = students_file
+    def find_highest_gwa(self):
+        highest_student = None
+        highest_gwa = 1.0
+        
 
