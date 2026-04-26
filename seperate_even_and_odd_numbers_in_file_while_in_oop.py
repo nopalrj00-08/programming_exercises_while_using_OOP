3888dc13cb047bd3c91b718cefff2654b1c41809
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-27 00:12:36 +0800

    create a method '

--- seperate_even_and_odd_numbers_in_file_while_in_oop.py
+++ seperate_even_and_odd_numbers_in_file_while_in_oop.py
@@ -1,2 +1,4 @@
-with(open("number.txt","r")) as my_file:
-    numbers = [int(line.strip()) for line in my_file if line.strip()]
+def main_process
+    with(open("number.txt","r")) as my_file:
+        numbers = [int(line.strip()) for line in my_file if line.strip()]
+
