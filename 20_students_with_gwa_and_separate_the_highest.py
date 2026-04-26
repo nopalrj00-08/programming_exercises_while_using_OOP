e4c87734107deb15d5180b71385ff690b5c554cb
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-27 03:44:23 +0800

    identify who's the highest

--- 20_students_with_gwa_and_separate_the_highest.py
+++ 20_students_with_gwa_and_separate_the_highest.py
@@ -4,5 +4,13 @@ class Separate:
     def find_highest_gwa(self):
         highest_student = None
         highest_gwa = 1.0
-        
+        with open(self.students_file,'r') as student_file:
+            for line in student_file:
+                line = line.strip()
+                name = line.split()[0]
+                gwa = float(line.split()[1])
+                if gwa > highest_gwa:
+                    highest_gwa = gwa
+                    highest_student = name
+                    
 
