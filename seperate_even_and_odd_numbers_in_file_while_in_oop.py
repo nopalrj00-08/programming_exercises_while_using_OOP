3ac03ff9702f26c2fac2c6825c652fb671b2dfd8
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-27 01:05:56 +0800

    changes

--- seperate_even_and_odd_numbers_in_file_while_in_oop.py
+++ seperate_even_and_odd_numbers_in_file_while_in_oop.py
@@ -1,16 +1,14 @@
 def main_process(my_file):
-    with(open("number.txt","r")) as my_file:
+    with(open("numbers.txt","r")) as my_file:
         numbers = [int(line.strip()) for line in my_file if line.strip()]
 
-    with(open("even_output.txt","w")) as even_file:
+    with(open("even_output.txt","w")) as even_file,open("odd_output.txt","w") as odd_file:
         for num in numbers:
             if num % 2 == 0:
                 even_file.write(f'{num}\n')
-
-    with(open("odd_output.txt","w")) as odd_file:
-        for num in numbers:
-            if num % 2 != 0:
+            else:
                 odd_file.write(f'{num}\n')
+
 
 
 
