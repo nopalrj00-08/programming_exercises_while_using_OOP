89dfccc5e56148f05ff45a36fd7732906d85a80f
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-28 03:04:46 +0800

    process codes

--- a_method_that_write_multiple_lines.py
+++ a_method_that_write_multiple_lines.py
@@ -1,6 +1,14 @@
+from numpy.ma.core import append
+
+
 class Writer:
     def __init__(self,writer_file):
         self.writer_file = writer_file
     def main_file(self):
         with open(self.writer_file,'w') as writer_data:
-            for line in writer_data:
+            print(f"Enter line:\033[32m{self.writer_file}\033[0m")
+        data_lines = []
+        while True:
+            lines_data = input("Enter line:\033[32m")
+            data_lines.append(lines_data)
+            choice_input = input("")
