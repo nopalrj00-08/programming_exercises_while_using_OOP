881def6999dc783830016af0db1dbe76367f500f
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-28 03:28:42 +0800

    finishing the code then after that, sleep

--- a_method_that_write_multiple_lines.py
+++ a_method_that_write_multiple_lines.py
@@ -3,18 +3,23 @@ class Writer:
             self.writer_file = writer_file
     def main_file(self):
         try:
-            with open(self.writer_file,'w') as writer_data:
-                print(f"Enter line:\033[32m{self.writer_file}\033[0m")
             data_lines = []
             while True:
-                lines_data = input("Enter line:\033[32m")
+                lines_data = input("Enter line: ")
                 data_lines.append(lines_data)
                 choice_input = input("Are there more lines y/n?")
                 if choice_input != "y":
                     break
+            with open(self.writer_file,'w') as writer_data:
+                for line in data_lines:
+                    writer_data.write(line + "\n")
         except FileNotFoundError:
             print("File not found")
         except PermissionError:
             print("Permission error")
         except ValueError:
             print("Value error")
+if __name__ == "__main__":
+    writer = Writer("writer.txt")
+    writer.main_file()
+
