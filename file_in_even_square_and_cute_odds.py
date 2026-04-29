54cfbf8722ef0af71540ee80a6e7ff6efb084b1f
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-29 23:36:38 +0800

    putting classs

---
+++ file_in_even_square_and_cute_odds.py
@@ -0,0 +1,5 @@
+class Seperator:
+    def __init__(self, even_squares, odds_cubes, integers_file):
+        self.even_squares = even_squares
+        self.odds_cubes = odds_cubes
+        self.integers_file = integers_file
