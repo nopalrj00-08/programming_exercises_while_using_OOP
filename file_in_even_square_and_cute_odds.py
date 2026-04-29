badd72f24aea625d36dcf3dc8c1a2510edd79f9d
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-30 00:16:38 +0800

    final output

--- file_in_even_square_and_cute_odds.py
+++ file_in_even_square_and_cute_odds.py
@@ -6,25 +6,34 @@ class Seperator:
         self.even_squares = even_squares
         self.odds_cubes = odds_cubes
         self.integers_file = integers_file
-    def prepare_file(self):
-        try:
+    try:
+        def prepare_file(self):
             with open(self.integers_file, "w") as int_file:
                 for integer in range(1,21):
                     int_file.write(f"{integer}\n")
-    def square_calculation(self):
-        with open(self.integers_file, "r") as even_file:
-            numbers = [int(line.strip()) for line in even_file if line.strip()]
-        with open(self.even_squares, "r") as even_squares_file:
-            for num in numbers:
-                if num % 2 == 0:
-                    even_squares_file.write(f"{num**2}\n")
-    def cube_calculation(self):
-        with open(self.integers_file, "r") as odd_file:
-            numbers = [int(line.strip()) for line in odd_file if line.strip()]
-        with open(self.odds_cubes, "r") as cube_file:
-            for num in numbers:
-                if num % 2 == 0:
-                    cube_file.write(f"{num**3}\n")
+        def square_calculation(self):
+            with open(self.integers_file, "r") as even_file:
+                numbers = [int(line.strip()) for line in even_file if line.strip()]
+            with open(self.even_squares, "r") as even_squares_file:
+                for num in numbers:
+                    if num % 2 == 0:
+                        even_squares_file.write(f"{num**2}\n")
+        def cube_calculation(self):
+            with open(self.integers_file, "r") as odd_file:
+                numbers = [int(line.strip()) for line in odd_file if line.strip()]
+            with open(self.odds_cubes, "r") as cube_file:
+                for num in numbers:
+                    if num % 2 == 0:
+                        cube_file.write(f"{num**3}\n")
+        print("Processing complete: even_output.txt and odd_output.txt files have been created.")
+    except FileNotFoundError:
+        print("File not found.")
+    except ValueError:
+        print("those are not integers.")
+if __name__ == "__main__":
+    number_seperator = Seperator("double.txt", "triple.txt", "integers.txt")
+    number_seperator.prepare_file()
+    number_seperator.square_calculation()
 
 
 
