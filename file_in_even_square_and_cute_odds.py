1948903ebb93396828282764f347be962c179005
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-29 23:45:48 +0800

    adding integer file

--- file_in_even_square_and_cute_odds.py
+++ file_in_even_square_and_cute_odds.py
@@ -3,3 +3,12 @@ class Seperator:
         self.even_squares = even_squares
         self.odds_cubes = odds_cubes
         self.integers_file = integers_file
+        def prepare_file(self):
+            with open(self.integers_file, "r") as int_file:
+                for integer in range(1,21):
+                    int_file.write(f"{integer}\n")
+
+
+
+
+
