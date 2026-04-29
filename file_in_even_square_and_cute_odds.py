9710523df4621b4c24c5f81837540e747da2e9d5
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-30 00:21:11 +0800

    final cleaning

--- file_in_even_square_and_cute_odds.py
+++ file_in_even_square_and_cute_odds.py
@@ -14,17 +14,12 @@ class Seperator:
         def square_calculation(self):
             with open(self.integers_file, "r") as even_file:
                 numbers = [int(line.strip()) for line in even_file if line.strip()]
-            with open(self.even_squares, "r") as even_squares_file:
+            with open(self.even_squares, "w") as even_squares_file, open(self.odds_cubes, "w") as odds_cubes_file:
                 for num in numbers:
                     if num % 2 == 0:
                         even_squares_file.write(f"{num**2}\n")
-        def cube_calculation(self):
-            with open(self.integers_file, "r") as odd_file:
-                numbers = [int(line.strip()) for line in odd_file if line.strip()]
-            with open(self.odds_cubes, "r") as cube_file:
-                for num in numbers:
-                    if num % 2 == 0:
-                        cube_file.write(f"{num**3}\n")
+                    else:
+                        odds_cubes_file.write(f"{num**3}\n")
         print("Processing complete: even_output.txt and odd_output.txt files have been created.")
     except FileNotFoundError:
         print("File not found.")
