fcd99ec0056ae8e3ddf2ff940bdc66adf2b6ff8d
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-30 00:07:08 +0800

    odd_cube calculations

--- file_in_even_square_and_cute_odds.py
+++ file_in_even_square_and_cute_odds.py
@@ -1,3 +1,6 @@
+from six import integer_types
+
+
 class Seperator:
     def __init__(self, even_squares, odds_cubes, integers_file):
         self.even_squares = even_squares
@@ -7,11 +10,20 @@ class Seperator:
         with open(self.integers_file, "w") as int_file:
             for integer in range(1,21):
                 int_file.write(f"{integer}\n")
-    def main_calculation(self):
-        with open(self.integers_file, "r") as my_file:
-            numbers = [int(line.strip()) for line in my_file if line.strip()]
+    def square_calculation(self):
+        with open(self.integers_file, "r") as even_file:
+            numbers = [int(line.strip()) for line in even_file if line.strip()]
         with open(self.even_squares, "r") as even_squares_file:
             for num in numbers:
+                if num % 2 == 0:
+                    even_squares_file.write(f"{num**2}\n")
+    def cube_calculation(self):
+        with open(self.integers_file, "r") as odd_file:
+            numbers = [int(line.strip()) for line in odd_file if line.strip()]
+        with open(self.odds_cubes, "r") as cube_file:
+            for num in numbers:
+                if num % 2 == 0:
+                    cube_file.write(f"{num**3}\n")
 
 
 
