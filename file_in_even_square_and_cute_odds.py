61e47f73a9cf07cc67f2b8d07951feeaa7f184dd
Reaven John E. Nopal <nopalrj00@gmail.com>
2026-04-29 23:56:31 +0800

    ready the main file

--- file_in_even_square_and_cute_odds.py
+++ file_in_even_square_and_cute_odds.py
@@ -3,10 +3,16 @@ class Seperator:
         self.even_squares = even_squares
         self.odds_cubes = odds_cubes
         self.integers_file = integers_file
-        def prepare_file(self):
-            with open(self.integers_file, "r") as int_file:
-                for integer in range(1,21):
-                    int_file.write(f"{integer}\n")
+    def prepare_file(self):
+        with open(self.integers_file, "w") as int_file:
+            for integer in range(1,21):
+                int_file.write(f"{integer}\n")
+    def main_calculation(self):
+        with open(self.integers_file, "r") as my_file:
+            numbers = [int(line.strip()) for line in my_file if line.strip()]
+        with open(self.even_squares, "r") as even_squares_file:
+            for num in numbers:
+
 
 
 
